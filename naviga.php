55c5ed187b84e0e921667af2566fba8216ab46f4
durnevaad <93126385+durnevaad@users.noreply.github.com>
2021-11-03 20:28:02 +0500

    Прописала навигацию для сайта

---
+++ naviga.php
@@ -0,0 +1,10 @@
+<title>Дурнева А.Д.</title>
+<ul>Задания:</ul>
+<li><a href="z1.html">Задание 1</a></li>
+<li><a href="z2.html">Задание 2</a></li>
+<li><a href="z3.html">Задание 3</a></li>
+<li><a href="z4.html">Задание 4</a></li>
+<li><a href="z5.html">Задание 5</a></li>
+<li><a href="z6.html">Задание 6</a></li>
+<li><a href="z7.php">Задание 7</a></li>
+<p><a href="../index.php">Вернуться назад</a></p>
